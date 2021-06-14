ab2240f2ca9a655f35638755cc9252cbf443b000
MariiaDrobotovych <70209190+MariiaDrobotovych@users.noreply.github.com>
2021-06-14 16:38:37 +0300

    fix task_3

--- js/task-3.js
+++ js/task-3.js
@@ -14,49 +14,17 @@ const images = [
   ];
 
 const galleryRef = document.querySelector('#gallery');
-galleryRef.style.display = 'flex';
+galleryRef.style.listStyle = 'none';
 
+const allItems = [];
 
 const createImage = (allImages) =>{
-    return allImages.map((image)=>{
-        const imageItem = document.createElement('img');
-        imageItem.url = image.url;
-        imageItem.alt = image.alt;
-        return galleryRef.insertAdjacentHTML("afterend",imageItem);;
+    return allImages.forEach((image)=>{
+        const item = `<li><img src= ${image.url} alt= ${image.alt} width='1260px'></li>`;
+        allItems.push(item);
     });
 }
     
 const createdImages = createImage(images);
-galleryRef.append(...createdImages);
 
-// const images = [
-//   {
-//     url:
-//       'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'White and Black Long Fur Cat',
-//   },
-//   {
-//     url:
-//       'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-//   },
-//   {
-//     url:
-//       'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'Group of Horses Running',
-//   },
-// ];
-
-// const galleryRef = document.querySelector('ul#gallery');
-
-// const createImgTag = images => {
-//   images.forEach(image => {
-//     const imageTag = document.createElement('img');
-//     imageTag.src = image.url;
-//     imageTag.alt = image.alt;
-//     galleryRef.appendChild(imageTag);
-//   });
-//   return galleryRef;
-// }
-
-// console.log(createImgTag(images)); 
+galleryRef.insertAdjacentHTML('beforeend', allItems.join(''));
